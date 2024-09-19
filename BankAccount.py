27f05ca793df80e7e6043a4880d40aab38b6fd66
agraber1@charlotte.edu <>
2024-09-19 17:23:08 -0400

    Added subclasses to BankAccount

--- BankAccount.py
+++ BankAccount.py
@@ -1,6 +1,6 @@
-# Abby Graber ITSC-3155 09/03/2024
+# Abby Graber ITSC-3155 09/19/2024
 
-class TitleBank():
+class TitleBank:
     # Class attribute
     bank_name = 'Title Bank'
 
@@ -32,10 +32,48 @@ class TitleBank():
 
     # Print Customer Information method
     def print_customer_information(self):
-        print("Bank: ", TitleBank.bank_name,
+        print("Bank:", TitleBank.bank_name,
               "\nCustomer Name:", self.customer_name,
               "\nCurrent Balance: $" + str(self.current_balance) +
               "\nMinimum Balance: $" + str(self.minimum_balance))
+
+
+class SavingsAccount(TitleBank):
+    def __init__(self, customer_name, current_balance, minimum_balance, interest_rate):
+        super().__init__(customer_name, current_balance, minimum_balance)
+        self.interest_rate = interest_rate
+
+    def interest_applied(self):
+        interest = self.current_balance * (self.interest_rate / 100)
+        self.current_balance += interest
+        print("Interest: $" + str(interest) + " New Balance: $" + str(self.current_balance))
+
+    def print_customer_information(self):
+        super().print_customer_information()
+        print("Interest Rate: " + str(self.interest_rate) + "%")
+
+
+class CheckingAccount(TitleBank):
+    def __init__(self, customer_name, current_balance, minimum_balance, transfer_limit):
+        super().__init__(customer_name, current_balance, minimum_balance)
+        self.transfer_limit = transfer_limit
+
+    def transfer(self, amount):
+        if amount > 0:
+            if amount > self.transfer_limit:
+                print("Transfer denied.")
+            elif (self.current_balance - amount) >= self.minimum_balance:
+                self.current_balance -= amount
+                print("Transferred: $" + str(amount) + " New Balance: $" + str(self.current_balance))
+            else:
+                print("Transfer denied.")
+        else:
+            print("Invalid transfer amount.")
+
+    def print_customer_information(self):
+        super().print_customer_information()
+        print("Transfer Limit:", self.transfer_limit)
+
 
 # Test instances
 c1 = TitleBank("Abby", 1000, 500)
@@ -43,7 +81,26 @@ c1.deposit(300)
 c1.withdraw(500)
 c1.print_customer_information()
 
+print()
+
 c2 = TitleBank("Gojo", 10000, 1000)
 c2.deposit(5000)
 c2.withdraw(1000)
 c2.print_customer_information()
+
+print()
+
+c3 = SavingsAccount("Sukuna", 3000, 900, 2.5)  # 2.5% interest rate
+c3.deposit(690)
+c3.withdraw(1000)
+c3.interest_applied()  # Apply interest to the balance
+c3.print_customer_information()
+
+print()
+
+c4 = CheckingAccount("Geto", 4000, 650, 1000)  # $1000 transfer limit
+c4.deposit(2000)
+c4.withdraw(300)
+c4.transfer(1500)
+c4.transfer(900)
+c4.print_customer_information()
